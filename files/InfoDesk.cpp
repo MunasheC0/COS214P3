01c589c9c94f15532abe0a8b0f64baaa83f38eda
Navela Hlungwani <navelahlungwani65@gmail.com>
2026-08-31 17:45:11 +0200

    Update InfoDesk.cpp and remove typo file ShuttleShop.h

--- files/InfoDesk.cpp
+++ files/InfoDesk.cpp
@@ -1,13 +1,10 @@
 #include "InfoDesk.h"
 #include <iostream>
 
-// This is the specific block of code the linker is looking for!
 InfoDesk::InfoDesk(std::string name, int capacity) : EventUnit(name, capacity) {
     currentSignage = "General Information"; // Initialize your private variable if you want
 }
 
-// Ensure your update method is also implemented here so you don't get another error!
 void InfoDesk::update(const Notice& notice) {
     std::cout << "[InfoDesk " << name << "] Received Notice: " << notice.message << "\n";
-    // Add any specific logic you need based on the NoticeType
 }
