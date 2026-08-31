7d7ee249299f45aee145c8d41bb281571d9a6f3f
Navela Hlungwani <navelahlungwani65@gmail.com>
2026-08-31 17:41:17 +0200

    Fix InfoDesk constructor to include capacity argument

--- files/InfoDesk.cpp
+++ files/InfoDesk.cpp
@@ -1,17 +1,13 @@
-#ifndef INFODESK_H
-#define INFODESK_H
+#include "InfoDesk.h"
+#include <iostream>
 
-#include "EventUnit.h"
-#include <string>
+// This is the specific block of code the linker is looking for!
+InfoDesk::InfoDesk(std::string name, int capacity) : EventUnit(name, capacity) {
+    currentSignage = "General Information"; // Initialize your private variable if you want
+}
 
-class InfoDesk : public EventUnit {
-private:
-    std::string currentSignage;
-
-public:
-    InfoDesk(std::string name, int capacity);
-    void update( const Notice& notice) override;
-    virtual ~InfoDesk() = default;
-};
-
-#endif
+// Ensure your update method is also implemented here so you don't get another error!
+void InfoDesk::update(const Notice& notice) {
+    std::cout << "[InfoDesk " << name << "] Received Notice: " << notice.message << "\n";
+    // Add any specific logic you need based on the NoticeType
+}
